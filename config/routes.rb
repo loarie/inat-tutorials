e10c65ed2901d9bafd5c65a533e58c4e773be019
Scott Loarie <loarie@gmail.com>
2015-11-13 22:09:04 -0800

    added multiple pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'tutorial#index'
+  root 'tutorial#inaturalist'
+  match '/observations', to: 'tutorial#observations', via: 'get'
+  match '/organisms', to: 'tutorial#organisms', via: 'get'
+  match '/identifications', to: 'tutorial#identifications', via: 'get'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
